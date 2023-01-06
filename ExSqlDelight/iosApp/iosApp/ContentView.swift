9ab8090a8b32116c6f605a6231722ad6e05d80b9
phucntt <vanphuc.earn.money@gmail.com>
2023-01-06 11:46:50 +0700

    finish_setup_ios

--- ExSqlDelight/iosApp/iosApp/ContentView.swift
+++ ExSqlDelight/iosApp/iosApp/ContentView.swift
@@ -2,11 +2,65 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greeting()
+	@ObservedObject private(set) var viewModel: ViewModel
 
-	var body: some View {
-		Text(greet)
-	}
+         var body: some View {
+             NavigationView {
+                 listView()
+                 .navigationBarTitle("SpaceX Launches")
+                 .navigationBarItems(trailing:
+                     Button("Reload") {
+                         self.viewModel.loadLaunches(forceReload: true)
+                 })
+             }
+         }
+
+         private func listView() -> AnyView {
+             switch viewModel.launches {
+             case .loading:
+                 return AnyView(Text("Loading...").multilineTextAlignment(.center))
+             case .result(let launches):
+                 return AnyView(List(launches) { launch in
+                     RocketLaunchRow(rocketLaunch: launch)
+                 })
+             case .error(let description):
+                 return AnyView(Text(description).multilineTextAlignment(.center))
+             }
+         }
+}
+
+extension ContentView {
+    enum LoadableLaunches {
+        case loading
+        case result([RocketLaunch])
+        case error(String)
+    }
+
+   class ViewModel: ObservableObject {
+       @Published var launches = LoadableLaunches.loading
+   }
+
+   class ViewModel: ObservableObject {
+           let sdk: SpaceXSDK
+           @Published var launches = LoadableLaunches.loading
+
+           init(sdk: SpaceXSDK) {
+               self.sdk = sdk
+               self.loadLaunches(forceReload: false)
+           }
+
+           func loadLaunches(forceReload: Bool) {
+               self.launches = .loading
+                       sdk.getLaunches(forceReload: forceReload, completionHandler: { launches, error in
+                           if let launches = launches {
+                               self.launches = .result(launches)
+                               } else {
+                                   self.launches = .error(error?.localizedDescription ?? "error")
+                               }
+                           })
+                       }
+           }
+   }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -14,3 +68,5 @@ struct ContentView_Previews: PreviewProvider {
 		ContentView()
 	}
 }
+
+extension RocketLaunch: Identifiable { }
